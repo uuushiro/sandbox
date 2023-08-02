8691a70ec6c5148e7c6671ebbdfc9321c96d3371
uuushiro <yushiro.ma2ta2.21@gmail.com>
2023-08-02 23:57:03 +0900

    filter map

--- rust/sample.rs
+++ rust/sample.rs
@@ -3,3 +3,12 @@ fn main() {
   printIn!("number is : {}", x)
   x = 6; // 再代入はコンパイルエラー
 }
+
+fn main() {
+  let source = vec![1,2,3,4,5];
+  let result = source
+    .into_iter()
+    .filter(|n| n % 2 == 0)
+    .map(|n| n.to_string())
+    .collect::<Vec<String>>();
+}
